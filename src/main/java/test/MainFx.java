8674408ee00a9d2038460f679877959285315b5a
MejryAzyz <med.azyz.mejry@gmail.com>
2025-02-22 01:16:28 +0100

    Home page v1

--- src/main/java/test/MainFx.java
+++ src/main/java/test/MainFx.java
@@ -16,7 +16,7 @@ public class MainFx extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        FXMLLoader loader=new FXMLLoader(getClass().getResource("/Home.fxml"));
+        FXMLLoader loader=new FXMLLoader(getClass().getResource("/TestHome.fxml"));
         Parent root = loader.load();
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
