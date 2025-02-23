654736494b49da68f274c88cce31be8e1eb5bf1a
elyes <msehli.elyes@esprit.tn>
2025-02-23 02:21:07 +0100

    Nouvelle interface hebergement

--- src/main/java/tests/Mainfx.java
+++ src/main/java/tests/Mainfx.java
@@ -16,7 +16,7 @@ public class Mainfx extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/GestionTransport.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/GestionHebergement.fxml"));
         Parent root = fxmlLoader.load();
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
